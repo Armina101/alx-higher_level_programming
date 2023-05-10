14b38841dd0d009bd27d32b1e30e738a7225b3ff
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-05-09 18:47:28 -0700

    More Debugging

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 10):
     for b in range(a + 1, 10):
-        print("{:d}{:d}".format(a, b), end="")
-        if a != 8 or b != 9:
-            print(",", end=" ")
-            print()
+        if a == 8 and b == 9:
+            print("{}{}".format(a, b))
+        else:
+            print("{}{}".format(a, b), end=", ")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for a in str:
-        if ord(a) >= 97 and ord(a) <= 122:
+        if ord(a) > 96:
             a = chr(ord(a) - 32)
-            print("{}".format(a), end="")
-            print()
+            print("{:s}".format(a), end='')
+            print('')
