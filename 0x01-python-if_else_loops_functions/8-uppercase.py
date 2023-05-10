7774f8cdfdcadb47034fa5d086428dfd669ffa23
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-05-09 17:52:21 -0700

    update6

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in str:
+        if ord(a) > 96:
+            a = chr(ord(a) - 32)
+            print("{:s}".format(a), end="")
+            print("")
