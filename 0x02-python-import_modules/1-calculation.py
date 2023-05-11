d5127c5a6006da8f4ac28bfe98cb989775d8e660
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-05-11 14:31:08 -0700

    Debugging

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    addit = add(a, b)
-    subtract = sub(a, b)
-    multiply = mul(a, b)
-    divide = div(a, b)
-    print(f"{a} + {b} = {addit}")
-    print(f"{a} - {b} = {subtract}")
-    print(f"{a} * {b} = {multiply}")
-    print(f"{a} / {b} = {divide}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
