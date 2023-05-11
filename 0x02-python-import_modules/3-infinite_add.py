f9b58ed895889cbba3514e8fe68f4ddb09845418
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-05-11 16:37:32 -0700

    Debugging2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     from sys import argv
-    arg_sum = 0
-    for n in range(len(argv) - 1):
-        arg_sum += (int(argv[n + 1]))
-        print("{:d}".format(arg_sum))
+    num = len(argv) - 1
+    if num == 0:
+        print(0)
+    else:
+        arg_sum = 0
+        for n in range(1, num + 1):
+            arg_sum += (int(argv[n]))
+            print(arg_sum)
