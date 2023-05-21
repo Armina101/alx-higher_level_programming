bae50c89b1f5f16fe0c8b5ed8f2d35e0d2135bdc
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-05-21 13:02:48 -0700

    Solving more Tasks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     list_length = len(my_list)
     if idx < 0:
         return (None)
-    else if idx > (list_length - 1):
+    if idx > (list_length - 1):
         return (None)
-    else
+
     return (my_list[idx])
