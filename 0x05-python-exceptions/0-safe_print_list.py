d8558869eb318ad03864e8d1d8107e9bbaad0eb2
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-05-29 18:41:42 -0700

    Debugging

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for a in range(x):
         try:
             print(my_list[a], end=" ")
-            n += 1
         except IndexError:
-            pass
-        print()
-        return n
+            break
+        else:
+            index += 1
+            print()
+            return index
